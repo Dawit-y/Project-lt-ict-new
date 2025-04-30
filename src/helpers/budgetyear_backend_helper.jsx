8e3c9fc66949d248aa46ce4af7aa372f80e1a3c1
Dawit Yimer <dawityimer52@gmail.com>
2025-04-30 09:35:02 +0300

    fix: backend helpers

--- src/helpers/budgetyear_backend_helper.jsx
+++ src/helpers/budgetyear_backend_helper.jsx
@@ -1,64 +1,34 @@
-import axios from "axios";
-import { del, get, post, put } from "./api_Lists";
-//import * as url from "./url_Lists";
+import { post } from "./api_Lists";
 
-const apiUrl = import.meta.env.VITE_BASE_API_URL;
 const GET_BUDGET_YEAR = "budget_year/listgrid";
 const ADD_BUDGET_YEAR = "budget_year/insertgrid";
 const UPDATE_BUDGET_YEAR = "budget_year/updategrid";
 const DELETE_BUDGET_YEAR = "budget_year/deletegrid";
 const POPULATE_BUDGET_YEAR = "budget_year/listdropdown";
-// get Projects
+
 export const getBudgetYear = async () => {
   try {
-    const response = await post(apiUrl + GET_BUDGET_YEAR);
+    const response = await post(GET_BUDGET_YEAR);
     return response;
   } catch (error) {
     throw error
   }
 };
-// get Projects
+
 export const populateBudgetYear = async () => {
   try {
-    const response = await post(apiUrl + POPULATE_BUDGET_YEAR);
+    const response = await post(POPULATE_BUDGET_YEAR);
     return response;
   } catch (error) {
     throw error
   }
 };
-// add Projects
-export const addBudgetYear = async (objectName) =>
-  post(`${apiUrl}` + ADD_BUDGET_YEAR, objectName);
-// update objectNames
-export const updateBudgetYear = (objectName) =>
-  post(
-    `${apiUrl}` + UPDATE_BUDGET_YEAR + `?bdy_id=${objectName?.bdy_id}`,
-    objectName
-  );
 
-// delete objectNames
-export const deleteBudgetYear = (objectName) =>
-  // post(`${url.DELETE_ORDER}?bdy_id=${order?.bdy_id}`);
-  post(`${apiUrl}` + DELETE_BUDGET_YEAR + `?bdy_id=${objectName}`);
+export const addBudgetYear = async (data) =>
+  post(ADD_BUDGET_YEAR, data);
 
-export const fetchSearchResults = async (searchTerm, selectedFields) => {
-  let queryParams = [];
-  if (searchTerm && searchTerm.search_en_value) {
-    queryParams.push(
-      `search_en_name=${encodeURIComponent(searchTerm.search_en_value)}`
-    );
-  }
-  selectedFields.forEach((field) => {
-    const [key] = Object.keys(field);
-    const value = field[key];
-    if (value !== undefined && value !== "") {
-      queryParams.push(`${key}=${encodeURIComponent(value)}`);
-    }
-  });
-  const queryString = queryParams.join("&");
-  const response = await axios.post(
-    `${apiUrl}budget_year/listgrid?${queryString}`
-  );
-  return response.data.data;
-};
-export { };
+export const updateBudgetYear = (data) =>
+  post(UPDATE_BUDGET_YEAR + `?bdy_id=${data?.bdy_id}`, data);
+
+export const deleteBudgetYear = (data) =>
+  post(DELETE_BUDGET_YEAR + `?bdy_id=${data}`);
--- src/helpers/stakeholdertype_backend_helper.jsx
+++ src/helpers/stakeholdertype_backend_helper.jsx
@@ -1,53 +1,24 @@
-import axios from "axios";
-import { del, get, post, put } from "./api_Lists";
-//import * as url from "./url_Lists";
+import { post } from "./api_Lists";
 
-const apiUrl = import.meta.env.VITE_BASE_API_URL;
 const GET_STAKEHOLDER_TYPE = "stakeholder_type/listgrid";
 const ADD_STAKEHOLDER_TYPE = "stakeholder_type/insertgrid";
 const UPDATE_STAKEHOLDER_TYPE = "stakeholder_type/updategrid";
 const DELETE_STAKEHOLDER_TYPE = "stakeholder_type/deletegrid";
-// get Projects
+
 export const getStakeholderType = async () => {
   try {
     const response = await post(GET_STAKEHOLDER_TYPE);
     return response;
   } catch (error) {
-    console.log(error); // Handle any errors
+    throw error
   }
 };
-// add Projects
-export const addStakeholderType = async (objectName) =>
-post(`${apiUrl}` + ADD_STAKEHOLDER_TYPE, objectName);
-// update objectNames
-export const updateStakeholderType = (objectName) =>
-  post(`${apiUrl}`+UPDATE_STAKEHOLDER_TYPE +`?sht_id=${objectName?.sht_id}`, objectName);
 
-// delete objectNames
-export const deleteStakeholderType = (objectName) =>
-  // post(`${url.DELETE_ORDER}?sht_id=${order?.sht_id}`);
-  post(`${apiUrl}`+DELETE_STAKEHOLDER_TYPE+`?sht_id=${objectName}`);
+export const addStakeholderType = async (data) =>
+  post(ADD_STAKEHOLDER_TYPE, data);
 
-export const fetchSearchResults = async (searchTerm, selectedFields) => {
-  let queryParams = [];
-  if (searchTerm && searchTerm.search_en_value) {
-    queryParams.push(
-      `search_en_name=${encodeURIComponent(searchTerm.search_en_value)}`
-    );
-  }
-  selectedFields.forEach((field) => {
-    const [key] = Object.keys(field);
-    const value = field[key];
-    if (value !== undefined && value !== "") {
-      queryParams.push(`${key}=${encodeURIComponent(value)}`);
-    }
-  });
-  const queryString = queryParams.join("&");
-  const response = await axios.post(
-    `${apiUrl}stakeholder_type/listgrid?${queryString}`
-  );
-  return response.data.data;
-};
-export {
-  
-};
+export const updateStakeholderType = (data) =>
+  post(UPDATE_STAKEHOLDER_TYPE + `?sht_id=${data?.sht_id}`, data);
+
+export const deleteStakeholderType = (data) =>
+  post(DELETE_STAKEHOLDER_TYPE + `?sht_id=${data}`);
